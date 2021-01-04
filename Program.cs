7ffc9858564a4ac33f83f6e101f1a2db188e1d0b
Kemilly Ferreira <73208400+KemillyFerreira@users.noreply.github.com>
2021-01-04 00:55:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pedir a operação
+            // Pedir o 1° número
+            // Pedir o 2° número
+
+            Console.WriteLine("Digite a operação que deseja fazer /n Soma, Subtracao, Multiplicacao, Divisao");
+            string operacao = Console.ReadLine();
+
+            Console.WriteLine("Digite o 1° número");
+            float num1 = float.Parse(Console.ReadLine());
+
+                //ALT + SHIFT + SETA PARA BAIXO
+
+            Console.WriteLine("Digite o 2° número");
+            float num2 = float.Parse(Console.ReadLine());
+
+            float resultado = 0;
+
+            //     //IF / ELSE PADRÂO
+            // if(operacao == "Soma"){
+            //     resultado = num1 + num2;
+            // }else if(operacao == "Subtracao"){
+            //     resultado = num1 - num2;
+            // }else if(operacao == "Multiplicacao"){
+            //     resultado = num1 * num2;
+            // }else if(operacao == "Divisao"){
+            //     resultado = num1 / num2;
+            // }else{
+            //     Console.WriteLine("Operação Inválida");
+            // }
+            // Console.WriteLine($"Resultado  : {resultado}");
+
+            switch(operacao){
+
+                case "Soma":
+                    resultado = num1+num2;
+                    break;
+                
+                case "Subtracao":
+                    resultado = num1+num2;
+                    break;
+
+                case "Multiplicacao":
+                    resultado = num1*num2;
+                    break;
+
+                case "Divisao":
+                    resultado = num1/num2;
+                    break;
+                
+                default:
+                    Console.WriteLine("Operação inválida");
+                    break;
+            }
+
+
+
+// Concatenação
+//Console.WriteLine("Calculo : "+num1+" e "+num2+" = Resultado" ; +resultado);
+
+// Interpolação
+Console.WriteLine($"Calculo: {num1} e {num2} = Resultado : {resultado}");
+
+        }
+    }
+}
+
